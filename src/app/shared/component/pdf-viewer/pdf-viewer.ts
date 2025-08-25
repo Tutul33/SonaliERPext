954ce52506853842ac9b79b251b1b181259f7c8e
Tutul Chakma <tutulcou@gmail.com>
2025-08-25 16:21:03 +0600

    Comit

--- src/app/shared/component/pdf-viewer/pdf-viewer.ts
+++ src/app/shared/component/pdf-viewer/pdf-viewer.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import { DialogModule } from 'primeng/dialog';
 import { PdfViewerService } from '../../services/pdf.viewer.service';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
 @Component({
   selector: 'app-pdf-viewer',
   standalone: true,
-  imports: [CommonModule, DialogModule,PdfViewerModule],
+  imports: [CommonModule, DialogModule, PdfViewerModule, NgxExtendedPdfViewerModule],
   templateUrl: './pdf-viewer.html',
 })
 export class PdfViewer {
+  service = inject(PdfViewerService);
 
-   service = inject(PdfViewerService);
+  constructor() {
+    pdfDefaultOptions.assetsFolder = 'assets';
+  }
 
-  
+  onDialogClose() {
+    this.service._visible = false; 
+  }
 }
--- src/app/shared/services/pdf.viewer.service.ts
+++ src/app/shared/services/pdf.viewer.service.ts
@@ -2,19 +2,19 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class PdfViewerService {
-  // PDF source (URL or Blob URL)
-  pdfSrc = signal<string | null>(null);
-
-  // Controls dialog visibility
-  visible = signal<boolean>(false);
+  _visible = false;
+  private _pdfSrc = '';
 
   open(src: string) {
-    this.pdfSrc.set(src);
-    this.visible.set(true);
+    this._pdfSrc = src;
+    this._visible = true;
   }
 
   close() {
-    this.visible.set(false);
-    this.pdfSrc.set(null);
+    this._visible = false;
+  }
+
+  pdfSrc() {
+    return this._pdfSrc;
   }
 }
